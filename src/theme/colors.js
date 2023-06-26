19fc5228f7b561d1281dd27c04ab37bb0f21beac
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-26 14:48:45 +0700

    setup colors and fonts

---
+++ src/theme/colors.js
@@ -0,0 +1,19 @@
+export const colors = {
+  primary: {
+    shade1: "#B65BFF",
+    shade2: "#D29AFF",
+    shade3: "#CD8EFF",
+  },
+  secondary: {
+    shade1: "#D5E1FF",
+    shade2: "#E7EEFF",
+    shade3: "#B9CDFF",
+  },
+  black: "#000B09",
+  grey: "#6A7B81",
+  stroke: "#E9E9E9",
+  green: "#00AE46",
+  white: "#FFFFFF",
+  darkStroke: "#27252D",
+  card: "#F6F7F8",
+};
