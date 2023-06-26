9fcc49b265320d01d27f8e879d000162e5b189d0
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-26 22:25:09 +0700

    slicing Creat Item

--- src/screens/CreateScreen.jsx
+++ src/screens/CreateScreen.jsx
@@ -14,7 +14,7 @@ export const CreateScreen = () => {
       <View style={styles.container}>
         <View style={styles.navigation}>
           <TouchableOpacity
-            style={styles.button}
+            style={styles.buttonIcon}
             onPress={() => navigation.goBack()}
           >
             <View style={styles.icon}>
@@ -22,14 +22,26 @@ export const CreateScreen = () => {
             </View>
           </TouchableOpacity>
         </View>
-        <View style={styles.wrapper}>
-          <Text style={styles.title}>Add new to-do</Text>
-          <TextInput style={styles.input} />
-        </View>
+        <View style={styles.wrapperContainer}>
+          <View style={styles.wrapper}>
+            <Text style={styles.title}>Add new to-do</Text>
+            <Text style={styles.description}>
+              Pleasae input your to-do below
+            </Text>
+          </View>
 
-        <View style={styles.wrapper}>
-          <Text style={styles.description}>Pleasae input your to-do below</Text>
-          <TextInput style={styles.input} />
+          <View style={styles.textInput}>
+            <TextInput placeholder="Title" style={styles.titleInput} />
+            <TextInput
+              placeholder="Description"
+              style={styles.descriptionInput}
+            />
+          </View>
+        </View>
+        <View style={styles.containerButton}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textButton}>Create new to-do</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
@@ -43,18 +55,98 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingBottom: 40,
     paddingHorizontal: 20,
+    backgroundColor: colors.white,
   },
-  container: {},
+  container: {
+    gap: 40,
+  },
   navigation: {
     width: 24,
     height: 24,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: colors.secondary.shade2,
   },
-  button: {},
-  icon: {},
-  wrapper: {},
-  title: {},
-  description: {},
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 6,
+  },
+  wrapperContainer: {
+    gap: 60,
+  },
+  title: {
+    color: colors.black,
+    textAlign: "center",
+    fontSize: 30,
+    fontFamily: "JosefinSans-Medium",
+    fontWeight: 600,
+    lineHeight: 36,
+    letterSpacing: -0.3,
+  },
+  description: {
+    color: colors.grey,
+    textAlign: "center",
+    fontSize: 14,
+    fontFamily: "JosefinSans-Medium",
+    lineHeight: 20,
+  },
+  button: {
+    width: 24,
+    height: 24,
+    flexShrink: 0,
+  },
+  textInput: {
+    gap: 16,
+  },
+  titleInput: {
+    display: "flex",
+    width: 335,
+    height: 48,
+    paddingVertical: 16,
+    paddingStart: 16,
+    paddingHorizontal: 217,
+    alignItems: "center",
+    gap: 3,
+    flexShrink: 0,
+    borderColor: "#E9E9E9",
+    borderWidth: 1,
+    borderStyle: "solid",
+    backgroundColor: "#fff",
+  },
+  descriptionInput: {
+    display: "flex",
+    width: 335,
+    height: 48,
+    paddingVertical: 16,
+    paddingStart: 16,
+    paddingHorizontal: 217,
+    alignItems: "center",
+    gap: 3,
+    flexShrink: 0,
+    borderColor: "#E9E9E9",
+    borderWidth: 1,
+    borderStyle: "solid",
+    backgroundColor: "#fff",
+  },
+  containerButton: {
+    marginVertical: 336,
+    alignItems: "center",
+  },
+  button: {
+    width: 335,
+    height: 48,
+    flexShrink: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primary.shade1,
+  },
+  textButton: {
+    color: colors.white,
+    fontSize: 14,
+    fontFamily: "JosefinSans-Medium",
+    fontWeight: 500,
+    lineHeight: 14,
+    letterSpacing: -0.14,
+  },
 });
