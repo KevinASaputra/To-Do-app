840adf0f16590647d0ca9d9c43f6a374b94448ff
Kevin <kevinadikasaputragithub@gmail.com>
2023-06-26 14:51:07 +0700

    setup and add icon svg

---
+++ src/component/Icons/index.js
@@ -0,0 +1,6 @@
+import MenuIcon from "./Menu.svg";
+import EditIcon from "./Edit.svg";
+import PlusIcon from "./Plus.svg";
+import BackIcon from "./Back.svg";
+
+export { MenuIcon, EditIcon, PlusIcon, BackIcon };
